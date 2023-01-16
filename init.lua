de4d9e2cb0358608e0aa1962df895c0bc0438cb7
Adam Beigel <31280510+abeige@users.noreply.github.com>
2023-01-15 23:08:02 -0600

    set options

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
+require "adam.options"
 
---
+++ lua/adam/options.lua
@@ -0,0 +1,36 @@
+local options = {
+	clipboard = "unnamedplus",
+	mouse = "a",
+	cursorline = true,
+	number = true,
+	relativenumber = true,
+	scrolloff = 5,
+	sidescrolloff = 5,
+	wrap = false,
+    splitbelow = true,
+    splitright = true,
+    signcolumn = "no",
+    smartcase = true,
+    hlsearch = true,
+    -- termguicolors = true,
+    -- signcolumn = "yes",
+    completeopt = { "menuone", "noselect" },
+
+	guicursor = "n-v-sm:hor20,i-ci-ve-c:ver25-Cursor,r-cr-o:block," ..
+                "a:blinkwait100-blinkon200-blinkoff150",
+
+	showtabline = 2,
+	expandtab = true,
+	shiftwidth = 4,
+	tabstop = 4,
+	smartindent = true,
+    autoindent = true,
+    softtabstop = 4,
+}
+
+for k,v in pairs(options) do
+	vim.opt[k] = v
+end
+
+vim.cmd [[set iskeyword+=-]]
+
